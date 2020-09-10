58f0ffef0b8efcbd767ad9ad632aba5e3530a72f
hsung8 <hsung@atlantaregional.org>
2020-09-09 23:21:33 -0400

    create folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Impossible Burger');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, ate) VALUES ('Spicy Chicken Burger', true);
+INSERT INTO burgers (name, ate) VALUES ('Bulgogi Burger', true);
+INSERT INTO burgers (name, ate) VALUES ('Whopper', true);
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
+INSERT INTO burgers (name) VALUES ('Deluxe Veggie Burger');
