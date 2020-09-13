9d88f1f53a020c4039e5d7e11472dcf302bc2025
hsung8 <hsung@atlantaregional.org>
2020-09-12 23:42:49 -0400

    edits

--- models/burger.js
+++ models/burger.js
@@ -13,10 +13,8 @@ var burger = {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
+  update: function(condition, cb) {
+    orm.update("burgers", {ate : 1}, condition, cb);
   },
   delete: function(condition, cb) {
     orm.delete("burgers", condition, function(res) {
